4a10382656b76f0b7ecd7ef4a581e6bd8fe8b6cb
FR-AKITO <varshakoli638@gmail.com>
2024-12-21 12:46:41 +0530

    Update start.py

--- start.py
+++ start.py
@@ -11,7 +11,7 @@ app = Client(
 
 @app.on_message(filters.command("start"))
 def start_command(client, message):
-    photo = "computer.jpg"
+    photo = "http://ibb.co/sRMYPzM"
     caption = "Soon"
     group_button = InlineKeyboardButton("Group", url="https://t.me/Anime_X_Isekai_Verse")
     channel_button = InlineKeyboardButton("Channel", url="https://t.me/Anime_X_Isekai")
