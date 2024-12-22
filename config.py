69281effc04287cf97b2848dd752b363d3e21d2b
𝐀𝐊𝐈𝐓𝐎 <samarkoli790@gmail.com>
2024-12-22 13:32:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = "23241529"
 api_hash = "f413b120fda101d2b833f132db2dbf54"
-bot_token = "7421954505:AAFKsNMzARZAJtjMhcuLu0m5ppQg1SlvzXY"
+bot_token = "7423326675:AAEB8DSl0Mqnj8xRxYwAdS2WYH_8AX-1EdY"
