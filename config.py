baf3e01b24e75e70e021af13330bdce67bdabe74
FR-AKITO <varshakoli638@gmail.com>
2024-12-22 12:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = "23241529"
 api_hash = "f413b120fda101d2b833f132db2dbf54"
-bot_token = "7423326675:AAE7QrJk213jKcdmiZspQ6EzOQHrY1fBHZQ"
+bot_token = "7421954505:AAFKsNMzARZAJtjMhcuLu0m5ppQg1SlvzXY"
